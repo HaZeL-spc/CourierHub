e13b45afe06a1b3110c12082e778065b30764b75
Manomenu <88382777+Manomenu@users.noreply.github.com>
2023-10-30 21:27:49 +0100

    cleanup

--- CourierAPI/Services/DeliveryRepository.cs
+++ CourierAPI/Services/DeliveryRepository.cs
@@ -2,52 +2,59 @@
 using CourierAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace CourierAPI.Services
+namespace CourierAPI.Services;
+
+public interface IDeliveryRepository
 {
-    public class DeliveryRepository : IDeliveryRepository
+    public Task<Result<IEnumerable<DeliveryModel>>> GetAllDeliveries();
+    public Task<Result<DeliveryModel>> GetDelivery(DeliveryModel deliveryId);
+    public Task<Result> AddDelivery(DeliveryModel employee);
+    public Task<Result> UpdateDelivery(DeliveryModel employee);
+    public Task<Result> DeleteDelivery(int deliveryId);
+}
+public class DeliveryRepository : IDeliveryRepository
+{
+    private readonly DeliverymanCastingDbContext _dbContext;
+
+    public DeliveryRepository(DeliverymanCastingDbContext dbContext)
     {
-        private readonly DeliverymanCastingDbContext _dbContext;
+        _dbContext = dbContext;
+    }
 
-        public DeliveryRepository(DeliverymanCastingDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+    public Task<Result> AddDelivery(DeliveryModel employee)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<Result> AddDelivery(DeliveryModel employee)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<Result> DeleteDelivery(int deliveryId)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<Result> DeleteDelivery(int deliveryId)
+    public async Task<Result<IEnumerable<DeliveryModel>>> GetAllDeliveries()
+    {
+        try
         {
-            throw new NotImplementedException();
+            var query = from d in _dbContext.Deliveries
+                        join el in _dbContext.Locations on d.EndLocation!.Id equals el.Id
+                        join sl in _dbContext.Locations on d.StartLocation!.Id equals sl.Id
+                        select new DeliveryModel(d.Id, d.Name, new LocationModel(sl), new LocationModel(el));
+            var deliveries = await query.ToListAsync();
+            return Result.Ok<IEnumerable<DeliveryModel>>(deliveries);
         }
+        catch
+        {
+            return Result.Fail<IEnumerable<DeliveryModel>>("Failed to recieve data");
+        }
+    }
 
-        public async Task<Result<IEnumerable<DeliveryModel>>> GetAllDeliveries()
-        {
-            try
-            {
-                var query = from d in _dbContext.Deliveries
-                            join el in _dbContext.Locations on d.EndLocation!.Id equals el.Id
-                            join sl in _dbContext.Locations on d.StartLocation!.Id equals sl.Id
-                            select new DeliveryModel(d.Id, d.Name, new LocationModel(sl), new LocationModel(el));
-                var deliveries = await query.ToListAsync();
-                return Result.Ok<IEnumerable<DeliveryModel>>(deliveries);
-            }
-            catch
-            {
-                return Result.Fail<IEnumerable<DeliveryModel>>("Failed to recieve data");
-            }
-        }
+    public Task<Result<DeliveryModel>> GetDelivery(DeliveryModel deliveryId)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<Result<DeliveryModel>> GetDelivery(DeliveryModel deliveryId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Result> UpdateDelivery(DeliveryModel employee)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<Result> UpdateDelivery(DeliveryModel employee)
+    {
+        throw new NotImplementedException();
     }
 }
