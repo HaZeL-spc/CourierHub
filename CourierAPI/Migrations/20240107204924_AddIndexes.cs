9c88048e589d03a5f26993e1cb0ea9ac05c00390
HaZeL-spc <kacper.leszczu8@gmail.com>
2024-01-08 20:56:30 +0100

    [91] added index on sessionhistory

---
+++ CourierAPI/Migrations/20240107204924_AddIndexes.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourierAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Locations_Id",
+                table: "Locations",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inquiries_Id",
+                table: "Inquiries",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliveries_Id",
+                table: "Deliveries",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Couriers_Id",
+                table: "Couriers",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_Id",
+                table: "Clients",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Locations_Id",
+                table: "Locations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Inquiries_Id",
+                table: "Inquiries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Deliveries_Id",
+                table: "Deliveries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Couriers_Id",
+                table: "Couriers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Clients_Id",
+                table: "Clients");
+        }
+    }
+}
---
+++ CourierCastingApp/Migrations/20240108195441_AddedIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourierCastingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SessionHistory_Id",
+                table: "SessionHistory",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SessionHistory_Id",
+                table: "SessionHistory");
+        }
+    }
+}
